7b290e5b5f91c0d1cf4bb332fa2549ed23e543f7
Shamil P. Gretzky <shamil.premalal@mail.mcgill.ca>
2018-04-04 22:18:21 -0400

    Potentiometer Fixed

--- StepperMotorProject.ino
+++ StepperMotorProject.ino
@@ -1,6 +1,4 @@
-//ItKindaWorks - Creative Commons 2016
-//github.com/ItKindaWorks
-//
+
 //Stepper motor control pins
 const int stepPin = 11;   //pin to pulse for steps
 const int dirPin = 10;    //pin to change step direction
@@ -8,9 +6,14 @@ const int dirPin = 10;    //pin to change step direction
 //User input pins 
 const int dispenserButton = 8;     // button that controls dispenser movement (digital pin)
 const int dispenserVar = 2; // button that controls dispensed amount (analog pin) 
+int dispencerONButton = 0;
+
 
 //Internal controls
 const int safetySwitch = 7; //internal switch used to limit the closing movement of the dispenser
+int safetyONButton = 0;
+int value = 0;
+int limit = 0;
 
 void setup() {
   
@@ -28,28 +31,40 @@ void setup() {
 
 void loop() {
 
+
   
   // Collect all data from user: dispenser limit & dispenser button
-  int dispencerONButton = digitalRead(dispenserButton);
-
+  dispencerONButton = digitalRead(dispenserButton);
+  value = analogRead(dispenserVar);          //Read and save analog value from potentiometer
+  value = map(value, 0, 1023, 0, 9000);      //Map value 0-1023 to 0-9000 (PWM)
 
   //limiter (internal control)
-  int safetyONButton = digitalRead(safetySwitch);
+  safetyONButton = digitalRead(safetySwitch);
 
   //Case user button is HIGH ==> Dispense regardless
-  if(dispencerONButton ==HIGH){
+  if(dispencerONButton == HIGH){
+     
       stepperFWD();
+      while(dispencerONButton == HIGH &&  limit <=value ){
       motorStep();
+      limit++;
+      }
     
     }
     else{ //Case is not pressed ==> close dispenser (until the internal button is hit)
-            stepperREV();
+        stepperREV();
       if(safetyONButton == HIGH){ //Case internal button is pressed ==> stop moving dispenser
      
         digitalWrite(stepPin, LOW);
         
-      }else{ //Case internal button has not yet been reached ==> continue closing
+        limit = 0;
+        
+      }else{ //Case internal button has not yet been reached ==> continue closing 
               motorStep();
+              limit --;
+              if(limit < 0){
+                  limit = 0;
+              }
        }
     }
 
